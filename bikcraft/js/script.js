71fd131a62bedb4c160b17c0040b1de349ef8ab8
Gabriella Oliveira <gaby999@outlook.com>
2021-05-27 09:41:20 -0300

    formulário

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -14,3 +14,12 @@ new SimpleSlide({
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email gabriellaodearaujo@gmail.com</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+}
